a9eb3f02b91d404fa2795282c0cfce83da42c6f4
mellv <melmat68@gmail.com>
2018-09-27 17:52:08 -0400

    Update knn.py
    Function to read and extract data.

--- knn.py
+++ knn.py
@@ -5,3 +5,15 @@ import numpy as np
 from math import sqrt
 from collections import Counter
 
+def data_exp(filename):
+    with open(filename, 'rb') as f:
+        null, d_type, dims = struct.unpack('>HBB', f.read(4))
+        data = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
+        if filename == "train-images.idx3-ubyte":
+            x = np.fromstring(f.read(), dtype=np.uint8).reshape(data[0], data[1] * data[2])
+            return x
+        if filename == "t10k-images.idx3-ubyte":
+            x = np.fromstring(f.read(), dtype=np.uint8).reshape(data[0], data[1] * data[2])
+            return x
+        y = np.fromstring(f.read(), dtype=np.uint8).reshape(data)
+        return y
